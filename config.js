6e45a7b5934300dfccd06f42358e1ac564a02b25
PeachTh <88878685+PeachTh@users.noreply.github.com>
2021-08-13 16:29:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introDesc: 'Quà thất tịch cho em nha cô gái',
+    btnIntro: '❤', 
+    title: 'Trời đổ mưa em đã đổ anh chưa',
+    desc: 'Đồng ý thì làm ny anh còn không thì làm vợ anh :3 ',
+    btnYes: 'Vãn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ❤',
+    btnReply: 'Gửi cho anh ❤',
+    reply: 'Why??',
+    mess: 'Anh biết mà 🥰. Anh tâm lý mà. Anh mới phát hiện ra mình còn giỏi yêu em nữa',
+    messDesc: 'Từ hôm nay em là công chúa của anh rồi. Call me oppa .',
+    btnAccept: ' Dạ ❤',
+    messLink: 'https://m.me/100009192941218' 
 }
